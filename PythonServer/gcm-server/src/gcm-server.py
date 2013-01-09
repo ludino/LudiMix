036d43a306b2af0df80dab7c4e1168d20877ec4e
Stian Fauskanger <stianfauskanger@gmail.com>
2013-01-09 14:39:08 +0100

    --cli --listusers --adduser --remuser --showuser is now working

--- PythonServer/gcm-server/src/gcm-server.py
+++ PythonServer/gcm-server/src/gcm-server.py
@@ -1,87 +1,131 @@
 #!/usr/bin/env python2
 
 import json
-import urllib2
 import sys
 import ludimix
 
 api_key = "AIzaSyDRHiFrhibsNqcAJwfb0jaFxv93GAgY-v0"
-reg_ids = {} 
-reg_ids['stian'] = 'APA91bHKF6_ACp20R3bZ3GF8_MYb_1icHWuTZ6Ja7UyyoW82P6Rr2gAo3qm-mEFjl3dQ2RLmMdGun0nI3jgnayNj96hrMbtBSyzh3Eve-bOA53F0JRUjXz8wHMT8EjT1RJxh1HvFs5m2qs92leuQUVAPxhppKKy3sQ'
-reg_ids['pelle'] = 'fkldjahs_lkjfdkjfhdaksjhfgdksjhgafkdsajhgfdkhgsakdh'
+user_db_file = '/Volumes/macdata/Users/stianfauskanger/test/gcm-server/users'
 
 def run_test(args):
-    user_db = ludimix.UserDatabase()
+    username = "stian"
+    user_db = ludimix.UserDatabase(user_db_file)
     user_db.load()
-    user_db.add("stian", "secret1", "fkdljashl", "dfjhsalk")
-    user_db.add("thomas", "secret2", "kjhfdla", "fdadsfd")
-    user_db.list()
+    user = user_db.find_by_name(username)
+    if user == None:
+        print "User not found."
+        run_listusers(args)
+        exit()
+    user.tokens.append("kjfhaslkjfhlsadjkhfsad")
+    user.tokens.append("utyrweoyturtiuorytowri")
+    user_db.save()
     
-    print user_db.save()
-
 def run_daemon(args):
     pass
 
 def run_cli(args):
-    pass
+    if len(args) < 2:
+        syntax()
 
-def run_listusers(args):
-    user_db = ludimix.UserDatabase()
-    user_db.load()
-    
-    user_db.add("stian", "secret1", "fkdljashl", "dfjhsalk")
-    user_db.add("thomas", "secret2", "kjhfdla", "fdadsfd")
-    
-    user_db.list()
+    user_db = ludimix.UserDatabase(user_db_file)
+    user_db.load()    
 
-def run_adduser(args):
-    pass
-
-def run_remuser(args):
-    pass
-
-def main(args):
+    to = args[0]
+    msg = ""
+    for word in args[1:]:
+        msg += word + " "
+    msg = msg.strip()
+     
     data = {}
-    data["ball"] = "teste leke bille kose"
-    data["kos"] = 34
+    data["msg"] = msg
     
-    to = ['stian', 'pelle']
-    
-    server = ludimix.Server(api_key, reg_ids)
-    result, status, data = server.send_data(to, data)
-    if result:
-        if not server.check_result(data):
-            server.handle_failed_result(to, data)
+    server = ludimix.Server(api_key, user_db)
+    result = server.send_data([to], data)
+    print type(result["error"])
+    if result["error"]:
+        print "Error: " + str(result["error_reason"])
+    else:
+        if not server.check_result(result):
+            server.handle_failed_result(result)
             print "Errors handled..."
         else:
             print "All ok"
-    else:
-        print "Error: " + str(data)
+
+def run_listusers(args):
+    user_db = ludimix.UserDatabase(user_db_file)
+    user_db.load()    
+    user_db.list()
+    
+def run_showuser(args):
+    if len(args) < 1:
+        syntax()
+    username = args[0]
+    user_db = ludimix.UserDatabase(user_db_file)
+    user_db.load()
+    user = user_db.find_by_name(username)
+    if user == None:
+        print "User not found."
+        run_listusers(args)
+        exit()
+    user.show()    
+
+def run_adduser(args):
+    if len(args) < 2:
+        syntax()
+    username, password = args[:2]
+    reg_ids = []
+    if len(args) >= 3:
+        reg_ids = [args[2]]
+    
+    user_db = ludimix.UserDatabase(user_db_file)
+    user_db.load()
+    if user_db.add(username, password, [], reg_ids):
+        user_db.save()
+        print "User '" + username + "' added"
+        exit()
+    print "User '" + username + "' already exists"
+
+def run_remuser(args):
+    if len(args) < 1:
+        syntax()
+    username = args[0]
+    user_db = ludimix.UserDatabase(user_db_file)
+    user_db.load()
+    user = user_db.find_by_name(username)
+    if user == None:
+        print "User not found."
+        run_listusers(args)
+        exit()
+    user_db.rem(user.user_id)
+    user_db.save()
+    print "User '" + username + "' removed" 
 
 def syntax():
     print "Syntax:"
-    print "gcm-server.py [option] [user] [msg|password]"
+    print "gcm-server.py [option] [user] [msg|password] [reg_id]"
     print "options:"
-    print "\t--daemon\t\t\tRun as a daemon"
-    print "\t--cli to msg\t\t\tSend msg to user once, and quit"
-    print "\t--listusers\t\t\tList users"
-    print "\t--adduser username password\tAdd user"
-    print "\t--remuser\t\t\tRemove users"
+    print "\t--daemon\t\t\t\tRun as a daemon"
+    print "\t--cli to msg\t\t\t\tSend msg to user once, and quit"
+    print "\t--listusers\t\t\t\tList users"
+    print "\t--showuser\t\t\t\tRemove users"
+    print "\t--adduser username password [reg_id]\tAdd user"
+    print "\t--remuser\t\t\t\tRemove users"
     exit()
 
-
-if __name__ == '__main__':
+def main():
     runAs = ""
     args = []
     if len(sys.argv) >= 2:
         runAs = sys.argv[1]
-        args = sys.argv[1:]
+        args = sys.argv[2:]
     if runAs == "--daemon":
         run_daemon(args)
     elif runAs == "--cli":
         run_cli(args)
     elif runAs == "--listusers":
         run_listusers(args)
+    elif runAs == "--showuser":
+        run_showuser(args)
     elif runAs == "--adduser":
         run_adduser(args)
     elif runAs == "--remuser":
@@ -90,7 +134,9 @@ if __name__ == '__main__':
         run_test(args)
     else:
         syntax()
-    #main()
+
+if __name__ == '__main__':
+    main()
     
     
     
--- PythonServer/gcm-server/src/ludimix/__init__.py
+++ PythonServer/gcm-server/src/ludimix/__init__.py
@@ -3,20 +3,33 @@ import json
 
 class Server(object):
 
-    def __init__(self, api_key, reg_ids):
+    def __init__(self, api_key, user_db):
         self.api_key = api_key
-        self.reg_ids = reg_ids
+        self.user_db = user_db
+        self.user_db.load()
         self.gcm_url = "https://android.googleapis.com/gcm/send"
 
     def send_data(self, to, data_content):
-        reg_id_list = []
+        ret = {}
+        ret["reg_id_list"] = []
+        ret["user_list"] = []
         for name in to:
-            if name in self.reg_ids:
-                key = self.reg_ids[name]
-                reg_id_list.append(key)
-    
+            user = self.user_db.find_by_name(name)
+            if not user == None:
+                key = user.reg_ids # TODO: CHECK THIS
+                if key == None or key == "":
+                    continue
+                ret["reg_id_list"].append(key)
+                ret["user_list"].append(user)
+        
+        if len(ret["reg_id_list"]) < 1:
+            ret["error"] = True
+            ret["status_code"] = 0
+            ret["error_reason"] = "NoRecipients"
+            return ret
+        
         data = {
-            'registration_ids' : reg_id_list,
+            'registration_ids' : ret["reg_id_list"],
             'data' : data_content
         }
     
@@ -31,22 +44,31 @@ class Server(object):
             data = json.loads(response.read())
             print "Data:"
             print data
-            return (True, 200, data)
+            ret["error"] = False
+            ret["status_code"] = 200
+            ret["data"] = data
+            return ret
         except urllib2.HTTPError as e:
             # read http://developer.android.com/google/gcm/gcm.html#response
-            return (False, e.code, e.reason)
+            ret["error"] = True
+            ret["status_code"] = e.code
+            ret["error_reason"] = e.reason
+            return ret
 
-    def check_result(self, data):
-        if data['failure'] == 0 and data['canonical_ids'] == 0:
+    def check_result(self, result):
+        if result["data"]['failure'] == 0 and result["data"]['canonical_ids'] == 0:
             return True
         return False
 
-    def handle_failed_result(self, to, data):
+    def handle_failed_result(self, result):
+        to = result["reg_id_list"]
+        users = result["user_list"]
+        data = result["data"]
         to_index = 0
         for result in data['results']:
             if 'message_id' in result:
                 if 'registration_id' in result:
-                    self.reg_ids[to[to_index]] = result['registration_id']
+                    self.reg_ids[to[to_index]] = result['registration_id'] # TODO: Check this
             else:
                 error = result['error']
                 if error == "MissingRegistration":
@@ -86,52 +108,130 @@ class User(object):
         self.user_id = None
         self.username = None
         self.password = None
-        self.token = None
-        self.reg_id = None
+        self.tokens = None
+        self.reg_ids = None
 
-    def setValues(self, user_id, username, password, token, reg_id):
-        self.user_id = user_id
+    def setValues(self, user_id, username, password, tokens, reg_ids):
+        self.user_id = int(user_id)
         self.username = username
         self.password = password
-        self.token = token
-        self.reg_id = reg_id    
+        self.tokens = tokens
+        self.reg_ids = reg_ids
+        
+    def toString(self):
+        string = {}
+        string["user_id"] = self.user_id
+        string["username"] = self.username
+        string["password"] = self.password
+        string["tokens"] = self.tokens
+        string["reg_ids"] = self.reg_ids
+        return json.dumps(string)
+    
+    def fromString(self, json_str):
+        string = json.loads(json_str)
+        self.user_id = int(string["user_id"]) 
+        self.username = string["username"]
+        self.password = string["password"] 
+        self.tokens = string["tokens"]
+        self.reg_ids = string["reg_ids"]
+        
+    def show(self):
+        print "User ID:\t\t%s" % self.user_id 
+        print "Username:\t\t%s" % self.username
+        print "Password:\t\t%s" % self.password
+        
+        if not len(self.tokens) == 0:
+            print "" 
+        p = "Security tokens" 
+        if len(self.tokens) == 0:
+            p += ": \t-"
+        else:
+            p += " (excl. ''):"
+            for token in self.tokens:
+                p += "\n* '" + token + "'"
+        print p
+        
+        if not len(self.reg_ids) == 0:
+            print "" 
+        p = "Register IDs" 
+        if len(self.reg_ids) == 0:
+            p += ": \t-"
+        else:
+            p += " (excl. ''):"
+            for reg_id in self.reg_ids:
+                p += "\n* '" + reg_id + "'"
+            p += "\n"
+        print p        
 
 class UserDatabase(object):
     
-    def __init__(self):
-        self.users_by_name = {}
-        self.users_by_id = {}
+    def __init__(self, path):
+        self._users_by_name = {}
+        self._users_by_id = {}
         self.next_id = 0
+        self.path = path
+        self.file = open(self.path, 'r+')
 
-    def add(self, username, password, token, reg_id):
+    def add(self, username, password, token, reg_ids):
+        if not self.find_by_name(username) == None:
+            return False
         new_user = User()
-        new_user.setValues(self.next_id, username, password, token, reg_id)
-        self.users_by_id[self.next_id] = new_user
-        self.users_by_name[username] = new_user
+        new_user.setValues(self.next_id, username, password, token, reg_ids)
+        self._users_by_id[self.next_id] = new_user
+        self._users_by_name[username] = new_user
         self.next_id += 1
+        return True
     
-    def rem(self, user):
-        user_id = user.user_id
+    def rem(self, user_id):
+        user = self.find_by_id(user_id)
+        if user == None:
+            return False
         username = user.username
-        del(self.users_by_id[user_id])
-        del(self.users_by_name[username])
+        del(self._users_by_id[user_id])
+        del(self._users_by_name[username])
+        return True
     
     def load(self):
-        pass
+        self._users_by_id = {}
+        self._users_by_name = {}
+        self.next_id = 0
+        self.file.seek(0)
+        read_data = self.file.readlines()
+        for json_str in read_data:
+            user = User()
+            user.fromString(json_str)
+            self._users_by_id[user.user_id] = user
+            self._users_by_name[user.username] = user
+            if user.user_id >= self.next_id:
+                self.next_id = user.user_id + 1
     
     def save(self):
-        return json.dumps(self.users_by_id)
+        save_data = ""
+        for user_id in self._users_by_id:
+            save_data += self._users_by_id[user_id].toString() + "\n"
+            
+        self.file.seek(0)        
+        self.file.truncate()
+        self.file.write(save_data)
     
     def list(self):
         print "Users:"
-        print "%s\t| %-30s\t\t| %s" % ("UserID", "Username", "Register ID")
-        print "-" * 100
-        for user_id in self._user_ids():
-            user = self.users_by_id[user_id]
-            print "%s\t| %-30s\t\t| %s" % (user.user_id, user.username, user.reg_id)
+        print "%s\t| %-30s\t\t| %s" % ("UserID", "Username", "Registered")
+        print "-" * 68
+        for user_id in self._users_by_id:
+            user = self._users_by_id[user_id]
+            reg_id = "No" if len(user.reg_ids) == 0 else "Yes"
+            print "%s\t| %-30s\t\t| %s" % (user.user_id, user.username, reg_id)
+            
+    def find_by_id(self, user_id):
+        if not user_id in self._users_by_id:
+            return None
+        return self._users_by_id[user_id]        
+        
+    def find_by_name(self, username):
+        if not username in self._users_by_name:
+            return None
+        return self._users_by_name[username]
     
-    def _user_names(self):
-        return self.users_by_name.keys()
     
-    def _user_ids(self):
-        return self.users_by_id.keys();
+    
