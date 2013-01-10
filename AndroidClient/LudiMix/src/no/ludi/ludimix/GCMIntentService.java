1949a9499fe2f749f80e2c5736a8bdcbb957a1c4
Stian Fauskanger <stianfauskanger@gmail.com>
2013-01-10 15:21:29 +0100

    A Toast is now showing when GCM messages is received

--- AndroidClient/LudiMix/src/no/ludi/ludimix/GCMIntentService.java
+++ AndroidClient/LudiMix/src/no/ludi/ludimix/GCMIntentService.java
@@ -4,6 +4,7 @@ import java.util.Set;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.widget.Toast;
 
@@ -11,6 +12,19 @@ import com.google.android.gcm.GCMBaseIntentService;
 
 
 public class GCMIntentService extends GCMBaseIntentService {
+
+	// // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // //
+	// http://stackoverflow.com/questions/4443278/toast-sending-message-to-a-handler-on-a-dead-thread  //
+	Handler mMainThreadHandler = null;  // // // // // // // // // // // // // // // // // // // // // //
+	// // // // // // // // // // // // WHY WHY WHY WHY WHY WHY WHY?? // // // // // // // // // // // //
+	// // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // //
+	
+	
+    public GCMIntentService() {
+        super(GCMIntentService.class.getName());
+
+        mMainThreadHandler = new Handler();
+    }
 
 	@Override
 	protected void onError(Context context, String errorId) {
@@ -31,10 +45,17 @@ public class GCMIntentService extends GCMBaseIntentService {
 		Bundle b = intent.getExtras();
 		Set<String> keys = b.keySet();
 		for (String key : keys) {
-			Object o = b.get(key);
+			final Object o = b.get(key);
 			Log.i(TAG, "onMessageMsg: k=" + key + " v=" + (String)o);
 			if (key.equals("msg")) {
-				Toast.makeText(getApplicationContext(), "MSG: " + (String)o, Toast.LENGTH_SHORT).show();
+
+				mMainThreadHandler.post(new Runnable() {
+		            @Override
+		            public void run() {
+		                Toast.makeText(getApplicationContext(), "Received: " + (String)o, Toast.LENGTH_LONG).show();
+		            }
+		        });
+				
 				Log.i(TAG, "ShouldToast");
 			}
 		}
